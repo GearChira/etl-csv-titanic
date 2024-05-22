04a3a6be01e04f5d61a3a9d5ec2971ae901d35f5
GearChira <chiranuvathn@gmail.com>
2024-05-22 16:38:33 +0700

    Update ETL script with more logic

--- etl-csv-script.py
+++ etl-csv-script.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import psycopg2 
 
 data = '/Users/chiranuvathn/Desktop/Development/etl-csv-titanic/train.csv'
+conn = psycopg2.connect("dbname=titanic user=postgres")
+cur = conn.cursor()
 
 def extract(data):
     return pd.read_csv(data)
@@ -26,3 +29,44 @@ def transform(extracted_data):
     extracted_data['LastName'] = last_names
 
     return extracted_data
+
+def create_staging_tb():
+    create_tb_query = """
+        CREATE TABLE IF NOT EXISTS staging_titanic (
+            passenger_id INTEGER NOT NULL,
+            is_survived INTEGER,
+            ticket_class INTEGER,
+            name VARCHAR(255),
+            gender VARCHAR(10),
+            age INTEGER,
+            no_of_sibling_or_spouse INTEGER,
+            no_of_parent_or_child INTEGER,
+            ticket_detail VARCHAR(50),
+            fare NUMERIC,
+            cabin_number VARCHAR(50),
+            port_of_embarked VARCHAR(10),
+            name_title TEXT,
+            name_first_name TEXT,
+            name_last_name TEXT,
+            PRIMARY KEY (passenger_id)
+        );
+    """
+
+    cur.execute(create_tb_query)
+    conn.commit()
+
+def insert_staging_tb(extracted_data):
+    for i, row in extracted_data.iterrows():
+        insert_query = f"""
+            INSERT INTO staging_titanic (passenger_id, is_survived, ticket_class, name, gender, age, no_of_sibling_or_spouse, 
+                no_of_parent_or_child, ticket_detail, fare, cabin_number, port_of_embarked, name_title, name_first_name, name_last_name)
+            VALUES ({row.passenger_id}, {row.is_survived}, {row.ticket_class}, '{row.name.replace("'", "''")}', '{row.gender}',
+                {row.age if not pd.isnull(row.age) else 'NULL'}, {row.no_of_sibling_or_spouse}, {row.no_of_parent_or_child},
+                '{row.ticket_detail.replace("'", "''")}', {row.fare}, '{row.cabin_number.replace("'", "''")}', 
+                '{row.point_of_embarked}', '{row.name_title}', '{row.name_first_name}', '{row.name_last_name}')
+            ON CONFLICT (passenger_id) DO NOTHING;
+        """
+    
+        cur.execute(insert_query)
+    conn.commit()
+
